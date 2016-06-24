b4b6ddd94b0ebf8bd034bf1d6fad32044c48392b
Sean Hayes <s00141688@mail.itsligo.ie>
2016-06-24 02:49:16 +0100

    Third Commit
    -Adding of GUI for testing
    -Adding of script for collectable (not fully working)
    -
    -All platform tilt (joined platforms)

---
+++ SMB_Remake/Assets/GUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        guiText =  "Hello Banana";
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        
+    }
+}
---
+++ SMB_Remake/Assets/MoveScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour
+{
+
+    //A bool to enable or disable smooth mode.
+    public bool smoothMotion = true;
+
+    //This is how quickly we MoveTowards the input axis.
+    public float smoothSpeed = 100f;
+
+    //The maximum we want our input axis to reach. Setting this lower will rotate the platform less, and higher will be more.
+    public float multiplier = 100f;
+    private float hSmooth = 0f;
+    private float vSmooth = 0f;
+    private float h;
+    private float v;
+
+    void Update()
+    {
+        //Get Vertical and Horizontal axis from Input
+        h = Input.GetAxis("Horizontal") * multiplier;
+        v = Input.GetAxis("Vertical") * multiplier;
+
+    }
+
+    void FixedUpdate()
+    {
+        hSmooth = Mathf.MoveTowards(hSmooth, h, smoothSpeed * Time.deltaTime);
+        vSmooth = Mathf.MoveTowards(vSmooth, v, smoothSpeed * Time.deltaTime);
+
+        //Rotate to match the new axis using EulerAngles(Must be used).
+        if (!smoothMotion)
+        {
+            transform.rotation = Quaternion.EulerAngles(new Vector3(h, 0f, v));
+        }
+        else
+        {
+            transform.rotation = Quaternion.EulerAngles(new Vector3(hSmooth, 0f, vSmooth));
+        }
+    }
+}
