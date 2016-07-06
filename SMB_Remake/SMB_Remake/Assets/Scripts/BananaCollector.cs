ede293b047d08595625bb5c0afb1fdf21ce3070a
Sean Hayes <s00141688@mail.itsligo.ie>
2016-07-07 00:17:53 +0100

    Commit 5.3
    -Continuous platform movement class (finally) (blog entry to come)
    -Fix of score counter
    -Deletion and tidy of collector class
    -Assets folders arranged

---
+++ SMB_Remake/SMB_Remake/Assets/Scripts/BananaCollector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BananaCollector : MonoBehaviour {
+
+    private int count = 0;
+    public Text bananaText;
+    public AudioClip bananaSound;
+
+    void Update()
+    {
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            AudioSource.PlayClipAtPoint(bananaSound, transform.position);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+
+    void SetCountText()
+    {
+        bananaText.text = "Bananas : " + count.ToString();
+    }
+}
---
+++ SMB_Remake/SMB_Remake/Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public Transform movingPlatform;
+    public Transform position1;
+    public Transform position2;
+    public Transform targetPosition;
+    public Vector3 newPosition;
+    private string currentState = ""; //stateholder
+    public float smooth;
+    public float resetTime;
+
+    //public enum POSITION { pos1, pos2}
+    //public Dictionary<POSITION, Vector3> positionList = new Dictionary<POSITION, Vector3>();
+
+    void Start()
+    {
+        SetTarget(position2);
+    }
+
+    void FixedUpdate()
+    {
+        //movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, smooth * Time.deltaTime);
+
+        movingPlatform.position =  movingPlatform.position + newPosition * smooth * Time.fixedDeltaTime;
+
+        if (Vector3.Distance(movingPlatform.position, targetPosition.position) < smooth * Time.fixedDeltaTime)
+        {
+            SetTarget(targetPosition == position1 ? position2 : position1);
+        }    
+    }
+
+
+    //Dictionary<POSITION, Vector3> positionList = new Dictionary<POSITION, Vector3>();
+    //Vector3 Position1 = position1.position;
+    //Vector3 Position2 = position2.position;
+    //positionList.Add(POSITION.pos1, Position1);
+    //positionList.Add(POSITION.pos2, Position2);
+
+    void SetTarget(Transform _targetPosition)
+    {
+        targetPosition = _targetPosition;
+        newPosition = (targetPosition.position - movingPlatform.position);
+    }
+}
